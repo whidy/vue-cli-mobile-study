8ca80b434d18e8dc2fd9f4b0c95698562b0564fe
Whidy <whidy@whidy.net>
2018-04-28 14:35:14 +0800

    父子组建数据传递测试分支

--- src/App.vue
+++ src/App.vue
@@ -2,12 +2,39 @@
   <div id="app" class="app">
     <img src="./assets/logo.png">
     <router-view/>
+    <button-bar :buttonList="buttonList1" @event-btn-a="clickBtnA" @event-btn-b="clickBtnB"></button-bar>
+    <button-bar :buttonList="buttonList2" @event-btn-c="clickBtnC"></button-bar>
   </div>
 </template>
 
 <script>
+import buttonBar from './components/buttonBar'
+
 export default {
-  name: 'App'
+  name: 'App',
+  components: {
+    'button-bar': buttonBar
+  },
+  data() {
+    return {
+      buttonList1: [{ title: '按钮A', eve: 'event-btn-a' }, { title: '按钮B', eve: 'event-btn-b' }],
+      buttonList2: [{ title: '按钮C', eve: 'event-btn-c' }]
+    }
+  },
+  methods: {
+    clickBtnA() {
+      console.log('clickBtnA')
+    },
+    clickBtnB() {
+      console.log('clickBtnB')
+    },
+    clickBtnC() {
+      console.log('clickBtnC')
+    }
+  },
+  created() {
+    console.log('created')
+  }
 }
 </script>
 
---
+++ src/components/buttonBar.vue
@@ -0,0 +1,37 @@
+<template>
+  <div class="button-bar">
+    <div class="button-bar-wrapper">
+      <button v-for="(btn, i) in buttonList" :key="i" class="btn" @click="$emit(btn.eve)">{{btn.title}}</button>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'button-bar',
+  props: ['buttonList'],
+  data() {
+    return {
+    }
+  }
+}
+</script>
+<style lang="postcss" scoped>
+.button-bar {
+  padding: 20px 0;
+  width: 100%;
+  background-color: #fff;
+  .button-bar-wrapper {
+    display:flex;
+  }
+  .btn {
+    flex:1;
+    border: 1px solid #eee;;
+    margin: 0 20px;
+    background-color: #fff;
+    font-size:24px;
+    line-height: 40px;
+    border-radius: 5px;
+  }
+}
+</style>
