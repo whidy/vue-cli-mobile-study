001bae4813e7b958087e7fe3fe4532be4c91c114
Whidy <whidy@whidy.net>
2018-05-07 11:58:17 +0800

    组件之间的数据交互研究

--- README.md
+++ README.md
@@ -1,1 +1,30 @@
-组件传递数据和事件
+# 组件传递数据及自定义多个事件, 多个参数测试
+
+该Demo包含以下知识点:
+
+* 父子组建使用
+* 数据通信
+* 事件自定义
+* 多参数测试
+* slot自定义结构
+* 底部固定容器自适应高度不遮挡内容区域功能
+
+等等细节不多说.
+
+不过这个demo的多参数传递, 这样写是否合理还有待考究.
+
+> **多参数**下如果直接写一个`params`, 他会把数字自动转成字符串, 比如`['furtrue', 'xixi']`, 如果写成
+
+```javascript
+console.log(`I also love whidy in the ${params}!`)
+console.log(`I also love whidy in the ${params[0]}, ${params[1]}!`)
+```
+
+输出结果几乎一致, 如下:
+
+```
+I also love whidy in the furtrue,xixi!
+I also love whidy in the furtrue, xixi!
+```
+
+所以还要进一步研究.
--- src/App.vue
+++ src/App.vue
@@ -1,39 +1,48 @@
 <template>
   <div id="app" class="app">
     <img src="./assets/logo.png">
-    <router-view/>
-    <button-bar :buttonList="buttonList1" @event-btn-a="clickBtnA" @event-btn-b="clickBtnB"></button-bar>
-    <button-bar :buttonList="buttonList2" @event-btn-c="clickBtnC"></button-bar>
+    <h1>你爱小白吗</h1>
+    <p class="tips">这是一个底部固定的自适应底部高度的小DEMO</p>
+    <bottom-fixed :buttonList="buttonList" v-on:isLoveA="loveA($event)" v-on:isLoveB="loveB($event)"></bottom-fixed>
   </div>
 </template>
 
 <script>
-import buttonBar from './components/buttonBar'
-
+import BottomFixed from './components/BottomFixed'
 export default {
   name: 'App',
   components: {
-    'button-bar': buttonBar
+    bottomFixed: BottomFixed
   },
   data() {
     return {
-      buttonList1: [{ title: '按钮A', eve: 'event-btn-a' }, { title: '按钮B', eve: 'event-btn-b' }],
-      buttonList2: [{ title: '按钮C', eve: 'event-btn-c' }]
+      buttonList: [
+        {
+          name: '是的',
+          type: 'primary',
+          iconClassName: 'icon-go-up',
+          event: { name: 'isLoveA', params: 'always' }
+        },
+        {
+          name: '超级爱',
+          type: 'danger',
+          iconClassName: 'icon-go-down',
+          event: { name: 'isLoveB', params: ['furtrue', 'xixi'] }
+        }
+      ]
     }
   },
   methods: {
-    clickBtnA() {
-      console.log('clickBtnA')
+    loveA(params) {
+      alert('I love whidy ' + params + '!')
+      // console.log('I love whidy ' + param + '!')
     },
-    clickBtnB() {
-      console.log('clickBtnB')
-    },
-    clickBtnC() {
-      console.log('clickBtnC')
+    loveB(params) {
+      // 多参数情况以下写法是否合理? 有待研究
+      alert(`I also love whidy in the ${params[0]}, ${params[1]}!`)
+      // console.log(`I also love whidy in the ${params}!`)
+      // console.log(`I also love whidy in the ${params[0]}, ${params[1]}!`)
     }
-  },
-  created() {
-    console.log('created')
   }
 }
 </script>
@@ -46,5 +55,18 @@ export default {
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
+}
+h1 {
+  font-size: 64px;
+  width: 64px;
+  line-height: 2;
+  padding: 1em;
+  margin: 1em auto;
+  border: 1px dashed #999;
+  word-break: break-all;
+}
+.tips {
+  color: #999;
+  font-size: 24px;
 }
 </style>
